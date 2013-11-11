ed3bb68d544770dfb7836b3e091933b83617bf67
unknown <abigailb@NEWGEN32W7X1650.us.corp>
2013-11-11 17:56:19 -0500

    first commit

---
+++ src/StoryQ.Converter.Wpf/Model/Parser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using StoryQ.Infrastructure;
+
+namespace StoryQ.Converter.Wpf.Model
+{
+    public static class Parser
+    {
+        private const StringComparison IgnoreCase = StringComparison.InvariantCultureIgnoreCase;
+
+        public static object Parse(IEnumerable<string> lines, object root)
+        {
+            int lineNumber = 1;
+            foreach (string untrimmedLine in lines)
+            {
+                string line = untrimmedLine.Trim();
+
+                if (string.IsNullOrEmpty(line)) continue;
+
+
+                var v = from m in GetMethods(root)
+                        from s in GetNames(m)
+                        orderby s.Length descending 
+                        where line.StartsWith(s, IgnoreCase)
+                        select new {Method = m, Name = s};
+
+                var match = v.FirstOrDefault();
+                if (match == null)
+                {
+                    string names = GetMethods(root).SelectMany(GetNames).Join(", ");
+                    string s = string.Format("expected one of [{1}]\nbut found '{0}'\nat line {2}", line, names, lineNumber);
+                    throw new ParseException(s);
+                }
+
+                string argument = line.Substring(match.Name.Length).Trim();
+                root = match.Method.Invoke(root, new object[] { argument });
+                lineNumber++;
+            }
+
+            return root;
+        }
+
+        static IEnumerable<string> GetNames(MethodInfo methodInfo)
+        {
+            yield return methodInfo.Name.UnCamel();
+            foreach (var a in Attribute.GetCustomAttributes(methodInfo).OfType<AliasAttribute>())
+            {
+                yield return a.Alias;
+            }
+        }
+
+        public static IEnumerable<MethodInfo> GetMethods(object o)
+        {
+            return from m in o.GetType().GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                   where m.ReturnType != typeof(void)
+                   where m.GetCustomAttribute<DescriptionAttribute>() != null
+                   where m.GetParameters().Select(x => x.ParameterType).SequenceEqual(new[] { typeof(string) })
+                   orderby m.Name.Length descending
+                   select m;
+        }
+
+        public class ParseException : ArgumentException
+        {
+            public ParseException(string message)
+                : base(message)
+            {
+            }
+        }
+    }
+
+}
---
+++ src/StoryQ/Execution/Rendering/TextRenderer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using StoryQ.Infrastructure;
+
+namespace StoryQ.Execution.Rendering
+{
+    internal class TextRenderer : IRenderer
+    {
+        readonly TextWriter output;
+
+        public TextRenderer(TextWriter output)
+        {
+            this.output = output;
+
+        }
+
+        public void Render(IEnumerable<Result> results)
+        {
+            StringWriter buffer = new StringWriter();
+            List<Exception> exceptionTable = results.Where(x => x.Type == ResultType.Failed).Select(x => x.Exception).ToList();
+            var messages = results.Select(r => new
+                            {
+                                Result = r,
+                                Description = new string(' ', 2 * r.IndentLevel) + r.Prefix + " " + r.Text
+                            }).ToList();
+
+            int messageLength = messages.Max(x => x.Description.Length);
+
+            foreach (var m in messages)
+            {
+                Result r = m.Result;
+
+                if (shouldPutNewlineBefore(r))
+                {
+                    buffer.WriteLine();
+                }
+
+                buffer.Write(m.Description);
+
+                if (r.Type != ResultType.NotExecutable)
+                {
+                    //padding
+                    buffer.Write(new string(' ', messageLength - m.Description.Length));
+                    buffer.Write(" => ");
+                    buffer.Write(r.Type);
+                    if(r.Type == ResultType.Pending)
+                    {
+                        buffer.Write(" !!");
+                    }
+                    else if (r.Type == ResultType.Failed)
+                    {
+                        buffer.Write(": \"");
+                        buffer.Write(r.Exception.Message);
+                        buffer.Write(" [");
+                        buffer.Write(exceptionTable.IndexOf(r.Exception) + 1);
+                        buffer.Write("]\"");
+                    }
+                }
+
+                var tags = r.Tags.Select(x => "#" + x).Join(", ");
+                if(!string.IsNullOrEmpty(tags))
+                {
+                    buffer.Write(" => (");
+                    buffer.Write(tags);
+                    buffer.Write(")");
+                }
+
+
+                buffer.WriteLine();
+            }
+
+            if (exceptionTable.Count > 0)
+            {
+                buffer.WriteLine();
+                buffer.WriteLine("_______________________");
+                buffer.WriteLine("Full exception details:");
+                buffer.WriteLine("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
+
+                for (int i = 0; i < exceptionTable.Count; i++)
+                {
+                    buffer.WriteLine("[{0}]: {1}", (i + 1), exceptionTable[i]);
+                    buffer.WriteLine();
+                }
+            }
+
+            output.Write(buffer.ToString());
+        }
+
+        protected virtual bool shouldPutNewlineBefore(Result r)
+        {
+            return r.IndentLevel == 3;
+        }
+    }
+}
